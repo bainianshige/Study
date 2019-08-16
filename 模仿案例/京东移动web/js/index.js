beb51c9cfe222208c671531329e100f247d20f04
firm <2597408801@qq.com>
2019-08-16 17:14:56 +0800

    2019/8/16下班提交

--- 模仿案例/京东移动web/js/index.js
+++ 模仿案例/京东移动web/js/index.js
@@ -5,7 +5,6 @@ window.onload = function () {
 }
 
 
-
 // 头部的js效果
 function searchEffect () {
 	// 头部搜索块的js效果
@@ -108,30 +107,179 @@ function bannerEffect() {
 
 		// 4.4设置默认的偏移
 		imgBox.style.left = - bannerWidth * index + "px";
-
 	}
 
-	// 5.实现自动播放
-	setInterval(function () {
-		// 5.1 变化索引
-		index++;
-		// 5.2 添加过渡效果
-		imgBox.style.transition = "left 0.5s ease-in-out"
-		// 5.3设置默认的偏移
-		imgBox.style.left = (-index * bannerWidth) + "px";
-		// 5.4 判断是否到最后一张
-		setTimeout(function () {
-			if (index == count-1) {
-				console.log(index);
-			index = 1;
-			// 如果一个元素的某个属性之前添加过过渡效果，那么过渡属性会一直存在，如果不想要，则需要清除过渡效果
-			// 关闭过渡效果
-			imgBox.style.transition = 'none';
-			// 偏移到制定的位置
-			imgBox.style.left = (-index * bannerWidth) + "px";
+	// 实现点标记
+	var setIndicator = function (index) {
+		var indicators = document.querySelector('.jd_bannerIndicator').querySelectorAll('li');
+		// 先清除其它li元素的active样式
+		for (i = 0; i < indicators.length; i++) {
+			indicators[i].classList.remove('active');
 		}
-		}, 500)
-	}, 2000)
+		// 为当前li元素添加active样式
+		indicators[index - 1].classList.add('active');
+	}
 
+	var timerId;
+	// 5.实现自动播放
+	var startTime = function startTime() {
+		timerId = setInterval(function () {
+			// 5.1 变化索引
+			index++;
+			// 5.2 添加过渡效果
+			imgBox.style.transition = "left 0.5s ease-in-out"
+			// 5.3设置默认的偏移
+			imgBox.style.left = (-index * bannerWidth) + "px";
+			// 5.4 判断是否到最后一张
+			setTimeout(function () {
+				if (index == count-1) {
+					// console.log(index);
+					index = 1;
+					// 如果一个元素的某个属性之前添加过过渡效果，那么过渡属性会一直存在，如果不想要，则需要清除过渡效果
+					// 关闭过渡效果
+					imgBox.style.transition = 'none';
+					// 偏移到制定的位置
+					imgBox.style.left = (-index * bannerWidth) + "px";
+				}
+			}, 500)
+		}, 2000)
+	}
+	startTime();
+
+	// 6.实现手动轮播
+	var startX, moveX, distanceX;
+	// 标记当前过渡效果是否已经执行完毕
+	var isEnd = true;
+	// 为图片添加触摸事件--触摸开始
+	imgBox.addEventListener('touchstart', function (e) {
+		// 清除定时器
+		clearInterval(timerId);
+		// 获取当前手指的起始位置
+		startX = e.targetTouches[0].clientX;
+	})
+	// 为图片添加触摸事件--滑动过程
+	imgBox.addEventListener('touchmove', function (e) {
+		if (isEnd == true) {
+			//获取手指滑动过程中的位置
+			moveX = e.targetTouches[0].clientX;
+			// 计算坐标的差异值
+			distanceX = moveX - startX;
+			// 为了保证效果正常，关闭过渡效果
+			imgBox.style.transition = 'none';
+			// 实现元素的偏移 left参照最原始的坐标
+			// 细节：本次滑动操作应该基于之前轮播图已经偏移的距离
+			imgBox.style.left = (-index * bannerWidth + distanceX) +'px';
+		}
+	});
+	// 为图片添加触摸事件--触摸结束
+	imgBox.addEventListener('touchend', function () {
+		// 自己琢磨的
+			// if (distanceX < - bannerWidth * 0.2) {
+			// 	index++;
+			// 	// 5.2 添加过渡效果
+			// 	imgBox.style.transition = "left 0.5s ease-in-out"
+			// 	// 5.3设置默认的偏移
+			// 	imgBox.style.left = (-index * bannerWidth) + "px";
+			// 	// 5.4 判断是否到最后一张
+			// 	setTimeout(function () {
+			// 		if (index == count-1) {
+			// 			console.log(index);
+			// 			index = 1;
+			// 			// 如果一个元素的某个属性之前添加过过渡效果，那么过渡属性会一直存在，如果不想要，则需要清除过渡效果
+			// 			// 关闭过渡效果
+			// 			imgBox.style.transition = 'none';
+			// 			// 偏移到制定的位置
+			// 			imgBox.style.left = (-index * bannerWidth) + "px";
+			// 		}
+			// 	}, 500)
+			// } else if (distanceX > bannerWidth * 0.3) {
+			// 	index--;
+			// 	// 5.2 添加过渡效果
+			// 	imgBox.style.transition = "left 0.5s ease-in-out"
+			// 	// 5.3设置默认的偏移
+			// 	imgBox.style.left = (-index * bannerWidth) + "px";
+			// 	// 5.4 判断是否到最后一张
+			// 	setTimeout(function () {
+			// 		if (index == count-1) {
+			// 			console.log(index);
+			// 			index = 1;
+			// 			// 如果一个元素的某个属性之前添加过过渡效果，那么过渡属性会一直存在，如果不想要，则需要清除过渡效果
+			// 			// 关闭过渡效果
+			// 			imgBox.style.transition = 'none';
+			// 			// 偏移到制定的位置
+			// 			imgBox.style.left = (-index * bannerWidth) + "px";
+			// 		}
+			// 	}, 500)
+			// } else {
+			// 	// 5.2 添加过渡效果
+			// 	imgBox.style.transition = "left 0.5s ease-in-out"
+			// 	// 5.3设置默认的偏移
+			// 	imgBox.style.left = (-index * bannerWidth) + "px";
+			// 	// 5.4 判断是否到最后一张
+			// 	setTimeout(function () {
+			// 		if (index == count-1) {
+			// 			console.log(index);
+			// 			index = 1;
+			// 			// 如果一个元素的某个属性之前添加过过渡效果，那么过渡属性会一直存在，如果不想要，则需要清除过渡效果
+			// 			// 关闭过渡效果
+			// 			imgBox.style.transition = 'none';
+			// 			// 偏移到制定的位置
+			// 			imgBox.style.left = (-index * bannerWidth) + "px";
+			// 		}
+			// 	}, 500)
+			// }
+
+		// 教程上的
+		// 松开手指，标记当前过渡效果正在执行
+		isEnd = false;
+		// 获取当前滑动的距离，判断距离是否超过指定的范围 bannerWidth * 0.2
+		if (Math.abs(distanceX) > 100) {
+			// 判断滑动的方向
+			if (distanceX > 0) { //上一张
+				index--;
+			} else { //下一张
+				index++;
+			}
+			// 翻页
+			imgBox.style.transition = 'left 0.5s ease-in-out';
+			imgBox.style.left = -index * bannerWidth + 'px';
+		} else if (Math.abs(distanceX) > 0) { //保证用户确实进行过滑动操作
+			// 回弹操作
+			imgBox.style.transition = 'left 0.5s ease-in-out';
+			imgBox.style.left = -index * bannerWidth + 'px';
+		}
+		// 将上一次move所产生的数据充值为0
+		startX = 0;
+		moveX = 0;
+		distanceX = 0;
+		//重新开启定时器
+		startTime();
+	});
+	// webkitTransitionEnd:可以监听当前元素的过渡效果执行完毕，当一个元素的过渡效果执行完毕的时候，会执行这个事件
+	imgBox.addEventListener('webkitTransitionEnd', function () {
+		// 如果到了最后一张(count-1)就回到索引1；如果到了第一张(0)就回到索引count-2
+		if (index == count-1) {
+			index = 1;
+			// 清除过渡
+			imgBox.style.transition = 'none';
+			// 设置偏移
+			imgBox.style.left = -index * bannerWidth + 'px';
+		} else if (index == 0) {
+			index = count-2
+			// 清除过渡
+			imgBox.style.transition = 'none';
+			// 设置偏移
+			imgBox.style.left = -index * bannerWidth + 'px';
+		}
+		// 设置标记
+		setIndicator(index);
+		setInterval(function () {
+			isEnd = true;
+			// // 清除定时器
+			// clearInterval(timerId);
+			// // 重新开启定时器
+			// startTime();
+		}, 200)
+	});
 }
 
