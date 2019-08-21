ec3b25d0164f950b2e5bb22094fd3905a74397ec
firm <2597408801@qq.com>
2019-08-21 17:14:49 +0800

    2019/8/21下班的提交

--- 课程练习/tom/data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-	$arr = array("username"=>"zhangsan","age"=>"12");
-
-	$cb = $_GET['callback'];
-	echo $cb.'('. json_encode($arr) .')';
-
- ?>
--- 课程练习/tom/json.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	
-	$cb = $_GET['cb'];
-	$arr = array("username"=>"zhangsan","age"=>"12");
-	echo $cb.'('. json_encode($arr) .')';
-
-	// echo "123";
-
- ?>
